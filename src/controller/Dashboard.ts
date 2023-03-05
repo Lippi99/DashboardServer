016af7931c5f0318d51c3cf91f3f8259d023c97f
Felipe Salomao <lipesalomaoclaf@gmail.com>
2023-03-05 10:54:18 -0300

    fix: fixed routes

--- src/controller/Dashboard.ts
+++ src/controller/Dashboard.ts
@@ -2,7 +2,6 @@ import { Request, Response } from "express";
 import moment from "moment";
 import { UserModel } from "../model/User";
 
-
 interface DateFilter {
   createdAt?: any;
   total: number;
@@ -10,57 +9,61 @@ interface DateFilter {
 
 export class Dashboard {
   async filter(req: Request, res: Response) {
-    const endingDate = new Date(req.query.endingDate as string);
+    try {
+      const endingDateStr = req.query.endingDate as string;
+      const endingDate = new Date(endingDateStr);
 
-    const currentDate = new Date();
-    currentDate.setDate(currentDate.getDate());
-    const user = await UserModel.aggregate<DateFilter>([
-      {
-        $match: {
-          createdAt: {
-            $gt: endingDate,
-            $lt: currentDate,
+      const currentDate = new Date();
+      currentDate.setHours(0, 0, 0, 0);
+
+      const userCounts = await UserModel.aggregate([
+        { $match: { createdAt: { $gte: endingDate, $lte: currentDate } } },
+        {
+          $group: {
+            _id: { $dateToString: { format: "%d/%m/%Y", date: "$createdAt" } },
+            count: { $sum: 1 },
           },
         },
-      },
-    ]);
+        { $sort: { _id: -1 } },
+      ]);
 
-    if (user.length == 0) {
-      res.status(404).json({ user: "not found" });
-     
-    } else {
-      user.sort((a, b) => b.createdAt - a.createdAt);
-      const countedData = user.reduce(
-        (acc: any, { createdAt }, index, array) => {
-          const date = moment(createdAt).format("L");
-          acc[`${date}`] = {
-            date,
-            total: (acc[`${date}`] ? acc[`${date}`].total : 0) + 1,
-        
-          };
-          return index === array.length - 1 ? Object.values(acc) : acc;
-        },
-        {}
-      );
-      res.status(200).json(countedData);
+      const formattedCounts = userCounts
+        .map(({ _id, count }) => ({
+          date: _id,
+          total: count,
+        }))
+        .sort((a, b) => {
+          const dateA: Date = new Date(a.date.split("/").reverse().join("/"));
+          const dateB = new Date(b.date.split("/").reverse().join("/"));
+          return Number(dateA) - Number(dateB);
+        });
+
+      res.json(formattedCounts);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
     }
   }
 
   async gender(req: Request, res: Response) {
-    const user = await UserModel.find({});
-    const countedData = user.reduce(
-      (acc: any, { gender }, index, array) => {
-       
-        acc[`${gender}`] = {
-          gender,
-          total: (acc[`${gender}`] ? acc[`${gender}`].total : 0) + 1,
-      
-        };
-        return index === array.length - 1 ? Object.values(acc) : acc;
-      },
-      {}
-    );
-  
-    res.status(200).send(countedData);
+    try {
+      const allGenders = await UserModel.aggregate([
+        {
+          $group: {
+            _id: "$gender",
+            total: {
+              $sum: 1,
+            },
+          },
+        },
+        { $project: { _id: 0, gender: "$_id", total: 1 } },
+        {
+          $sort: {
+            total: -1,
+          },
+        },
+      ]);
+      res.send(allGenders);
+    } catch (error) {}
   }
 }
